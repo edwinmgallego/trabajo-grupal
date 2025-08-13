fb35d894b1597bc380298a578eee876547012cc1
edwinmgallego <ing.edwin.gallego@gmail.com>
2025-08-13 16:57:04 -0500

    trabajo colaborativo

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.style.display = navLinks.style.display === "flex" ? "none" : "flex";
+});
