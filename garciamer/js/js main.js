1a586451187b87209ca69b875d23286478529e93
garciajefferson177-estar <garciajefferson177@gmail.com>
2025-08-13 20:52:45 -0500

    modificando el programa

---
+++ garciamer/js/js main.js
@@ -0,0 +1,6 @@
+const hamburger= document.getElementById("hamburger");
+const nav= document.getAnimations("nav");
+
+hamburger.addEventListener("click", () =>{
+    nav.classlist.toggle("active");
+})
